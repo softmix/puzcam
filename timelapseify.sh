3842e843ed522bfe30fa06f12a14b0b3b09cbc66
softmix <mindmix@gmail.com>
2023-12-27 22:17:31 +0100

    some cleanup

--- timelapseify.sh
+++ timelapseify.sh
@@ -2,9 +2,8 @@
 
 # Constants
 desired_file_size_mb=1.8
-kilobits_per_mb=8192
-initial_bitrate_kbps=250  # Starting bitrate, adjust as needed
-desired_duration=60       # Desired duration in seconds
+initial_bitrate_kbps=250  # Starting bitrate, usually works well on the first try
+desired_duration_sec=60   # Desired video duration in seconds
 
 # Check if a specific subdirectory is given as an argument
 if [ $# -eq 1 ]; then
@@ -33,32 +32,24 @@ for dir in "${directories[@]}"; do
     if ls chunk_*.webm 1> /dev/null 2>&1; then
         echo "Processing in directory: $dir"
 
-        # Concatenate chunks
-        temp_output_file="temp_merged_output.webm"
-        final_output_file="merged_output.webm"
-        > "$temp_output_file"
+        # Concatenate chunks into a single file
+        merged_chunks_file="merged_chunks_output.webm"
+        > "$merged_chunks_file"
 
         for chunk in $(ls chunk_*.webm | sort -V); do
-            cat "$chunk" >> "$temp_output_file"
+            cat "$chunk" >> "$merged_chunks_file"
         done
 
-        # Repackage the video file
-        ffmpeg -y -loglevel warning -i "$temp_output_file" -c:v copy -c:a copy "$final_output_file"
+        # Repackage the video file with fixed duration
+        duration_adjusted_file="duration_adjusted_output.webm"
+        ffmpeg -y -loglevel warning -i "$merged_chunks_file" -c:v copy -c:a copy "$duration_adjusted_file"
 
         # Calculate total duration of the video in seconds
-        total_duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$final_output_file")
-
-        # Check if total_duration is a valid number
-        if ! [[ $total_duration =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
-            echo "Invalid total duration for $dir. Skipping."
-            cd ..
-            continue
-        fi
-
+        total_duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$duration_adjusted_file")
         total_duration=$(printf "%.0f" "$total_duration")  # Convert to integer
 
         # Calculate speed-up factor
-        factor=$(echo "$total_duration / $desired_duration" | bc -l)
+        factor=$(echo "$total_duration / $desired_duration_sec" | bc -l)
         factor=$(printf "%.2f" "$factor")  # Format to 2 decimal places
 
         # Ensure factor is greater than zero
@@ -70,12 +61,10 @@ for dir in "${directories[@]}"; do
 
         echo "Factor calculated: $factor"
         echo "Total duration: $total_duration seconds"
-        echo "Desired duration: $desired_duration seconds"
-        echo "Initial bitrate: ${initial_bitrate_kbps}kbps"
-        echo "Desired file size: ${desired_file_size_mb}MB"
+
 
         # Detect crop parameters using Python script
-        python3 ../crop_detector.py "$final_output_file"
+        python3 ../crop_detector.py "$duration_adjusted_file"
         read -r x y w h < crop_params.txt
 
         # Go back to the parent directory
@@ -83,16 +72,16 @@ for dir in "${directories[@]}"; do
 
         # Adjust bitrate based on the output file size
         for (( i=0; i<5; i++ )); do
-            final_output="${dir%/}.webm"
+            final_encoded_file="${dir%/}.webm"
 
             echo "Iteration $i: Bitrate = ${initial_bitrate_kbps}kbps"
 
             # Two-pass encoding with current bitrate and scaling
-            ffmpeg -y -loglevel warning -i "$dir$final_output_file" -vf "setpts=PTS/$factor,crop=${w}:${h}:${x}:${y},scale='min(iw,2048)':'min(ih,2048)':force_original_aspect_ratio=decrease" -r 30 -c:v libvpx-vp9 -b:v ${initial_bitrate_kbps}k -pass 1 -vsync vfr -an -f webm /dev/null && \
-            ffmpeg -y -i "$dir$final_output_file" -vf "setpts=PTS/$factor,crop=${w}:${h}:${x}:${y},scale='min(iw,2048)':'min(ih,2048)':force_original_aspect_ratio=decrease" -r 30 -c:v libvpx-vp9 -b:v ${initial_bitrate_kbps}k -pass 2 -vsync vfr -an "$final_output"
+            ffmpeg -y -loglevel warning -i "$dir$duration_adjusted_file" -vf "setpts=PTS/$factor,crop=${w}:${h}:${x}:${y},scale='min(iw,2048)':'min(ih,2048)':force_original_aspect_ratio=decrease" -r 30 -c:v libvpx-vp9 -b:v ${initial_bitrate_kbps}k -pass 1 -vsync vfr -an -f webm /dev/null && \
+            ffmpeg -y -i "$dir$duration_adjusted_file" -vf "setpts=PTS/$factor,crop=${w}:${h}:${x}:${y},scale='min(iw,2048)':'min(ih,2048)':force_original_aspect_ratio=decrease" -r 30 -c:v libvpx-vp9 -b:v ${initial_bitrate_kbps}k -pass 2 -vsync vfr -an "$final_encoded_file"
 
             # Check file size
-            actual_size_kb=$(du -k "$final_output" | cut -f1)
+            actual_size_kb=$(du -k "$final_encoded_file" | cut -f1)
             actual_size_mb=$(echo "scale=2; $actual_size_kb/1024" | bc)
 
             echo "Iteration $i: File size = ${actual_size_mb}MB with bitrate = ${initial_bitrate_kbps}kbps"
@@ -121,7 +110,6 @@ for dir in "${directories[@]}"; do
         echo "Finished processing in directory: $dir"
     else
         echo "No chunk files in directory: $dir"
-        # Go back to the parent directory
         cd ..
     fi
 done
